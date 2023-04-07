bd4fb3197a48c62126e663cca915d70daa3033ee
Anushree1291 <95995815+Anushree1291@users.noreply.github.com>
2023-04-07 16:30:06 +0530

    Time: 7 ms (97.85%), Space: 55 MB (49.16%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int numEnclaves(int[][] g) {
+        for(int i=0;i<g.length;i++){
+            if(g[i][0]==1){
+                dfs(i,0,g);
+            }
+        }
+        for(int i=0;i<g.length;i++){
+            if(g[i][g[0].length-1]==1){
+                dfs(i,g[0].length-1,g);
+            }
+        }
+        for(int i=0;i<g[0].length;i++){
+            if(g[0][i]==1){
+                dfs(0,i,g);
+            }
+        }
+        for(int i=0;i<g[0].length;i++){
+            if(g[g.length-1][i]==1){
+                dfs(g.length-1,i,g);
+            }
+        }
+        int c=0;
+        for(int i=0;i<g.length;i++){
+            for(int j=0;j<g[0].length;j++){
+                c+=g[i][j];
+            }
+        }
+        return c;
+    }
+    public void dfs(int i,int j,int[][] g){
+        if(i==-1 || j==-1 || i==g.length || j==g[0].length || g[i][j]==0){
+            return;
+        }
+        g[i][j]=0;
+        dfs(i+1,j,g);
+        dfs(i,j+1,g);
+        dfs(i-1,j,g);
+        dfs(i,j-1,g);
+    }
+}
