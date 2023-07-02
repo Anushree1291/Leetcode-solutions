023392d57c1eac5db4d76b83cae0843dd130849d
Anushree1291 <95995815+Anushree1291@users.noreply.github.com>
2023-07-02 16:29:07 +0530

    Time: 1297 ms (60.19%), Space: 16.4 MB (53.40%) - LeetHub

---
+++ 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maximumRequests(self, n: int, r: List[List[int]]) -> int:
+        self.a=0
+        
+        def tra(ind,c):
+            #print(ind)
+            if(ind>=len(r)):
+                for i in range(n):
+                    if inx[i]!=0:
+                        return;
+                self.a=max(self.a,c)
+                return
+            
+            inx[r[ind][0]]-=1
+            inx[r[ind][1]]+=1
+            tra(ind+1,c+1)
+            
+            inx[r[ind][0]]+=1
+            inx[r[ind][1]]-=1
+            tra(ind+1,c)
+            return
+        
+        
+        inx=[0]*n
+        tra(0,0)
+        return self.a
