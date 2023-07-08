fc16c67fff9324bd93ae2eafff49386f936fb29f
Anushree1291 <95995815+Anushree1291@users.noreply.github.com>
2023-07-08 13:26:07 +0530

    Added solution - LeetHub

---
+++ Find triplets with zero sum - GFG/find-triplets-with-zero-sum.py
@@ -0,0 +1,50 @@
+#User function Template for python3
+
+class Solution:
+    #Function to find triplets with zero sum.    
+    def findTriplets(self, arr, n):
+        #code here
+        arr.sort()
+        for  i in range(n):
+            s=i+1
+            e=n-1
+            t=-arr[i]
+            while(s<e):
+                if(arr[s]+arr[e]==t):
+                    return 1
+                elif(arr[s]+arr[e]>t):
+                    e-=1
+                else:
+                    s+=1
+        return 0
+            
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+if __name__=='__main__':
+    t = int(input())
+    for i in range(t):
+        n=int(input())
+        a=list(map(int,input().strip().split()))
+        if(Solution().findTriplets(a,n)):
+            print(1)
+        else:
+            print(0)
+# } Driver Code Ends
