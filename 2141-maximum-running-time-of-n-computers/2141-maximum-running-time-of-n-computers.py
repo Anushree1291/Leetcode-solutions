d7088a7d9be161e8c763e4a2e9069212d62a6ecb
Anushree1291 <95995815+Anushree1291@users.noreply.github.com>
2023-07-27 22:27:23 +0530

    Time: 482 ms (100.00%), Space: 30.8 MB (28.92%) - LeetHub

---
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxRunTime(self, n: int, b: List[int]) -> int:
+        b.sort()
+        extra=sum(b[:-n])
+        live=b[-n:]
+        
+        for i in range(n-1):
+            
+            if extra//(i+1) < live[i+1]-live[i]:
+                return live[i]+extra//(i+1)
+            
+            extra-=(i+1)*(live[i+1]-live[i])
+            
+        return live[-1]+extra//n
